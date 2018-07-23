926d8ab1fa012454a25e3eb844bdeaf666766b64
boris tumbev <b.tumbev@code-nest.com>
2018-07-23 15:30:37 +0300

    created Vehicle model

---
+++ car_dealer_project/car_dealer_app/choices.py
@@ -0,0 +1,42 @@
+#from enum import Enum
+
+conditions         = (('N','New'),
+                      ('U','Used'))
+
+transmission_types = (('A','Auto'),
+                      ('M','Manual'),
+                      ('S','Semi-auto')) 
+
+engine_types       = (('D','Diesl'),
+                      ('P','Petrol'),
+                      ('E','Electric'),
+                      ('H','Hybrid'))
+
+vehicle_types      = (('A','Automobile'),
+                     ('M','Motorcycle'),
+                     ('B','Bus'),
+                     ('T','Truck'))
+
+
+
+# class Conditions(Enum):
+#     New  = 'New'
+#     Used = 'Used'
+
+# class TransmissionTypes(Enum):
+#     Auto      = 'Auto'
+#     Manual    = 'Manual'
+#     Semi_auto = 'Semi-auto'
+
+# class EngineTypes(Enum):
+#     Diesl    = 'Disel'
+#     Petrol   = 'Petrol'
+#     Electric = 'Electric'
+#     Hybrid   = 'Hybrid'
+
+# class VehicleTypes(Enum):
+#     Automobile = 'Automobile'
+#     Motorcycle = 'Motorcycle'
+#     Bus        = 'Bus'
+#     Truck      = 'Truck'
+
--- car_dealer_project/car_dealer_app/models.py
+++ car_dealer_project/car_dealer_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+import datetime
+from .choices import *
 
-# Create your models here.
+
+curr_year = datetime.datetime.now().year.__str__()
+
+class Vehicle(models.Model):
+
+    brand        = models.CharField(max_length=15)
+    model        = models.CharField(max_length=15)
+    v_type       = models.CharField(max_length=2,choices=vehicle_types)
+    engine_type  = models.CharField(max_length=1,choices=engine_types)
+    transmission = models.CharField(max_length=1,choices=transmission_types)
+    condition    = models.CharField(max_length=1,choices=conditions)
+
+    year         = models.PositiveIntegerField(validators=[MaxValueValidator(curr_year)])
+    price        = models.PositiveIntegerField()
+
+    #image        = models.ImageField(upload_to = 'images/')
+    sold         = models.BooleanField(default=False)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    description  = models.TextField()
