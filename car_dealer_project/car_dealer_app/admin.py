418460c3fde37d5ffa1a0c8a238462550b939e82
boris tumbev <b.tumbev@code-nest.com>
2018-08-07 17:48:45 +0300

    register msges in admin panel

--- car_dealer_project/car_dealer_app/admin.py
+++ car_dealer_project/car_dealer_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RentalVehicle, Make, Model, SellVehicle,MyUser,Log
+from .models import RentalVehicle, Make, Model, SellVehicle,MyUser,Log,Message
 
 admin.site.register(SellVehicle)
 admin.site.register(RentalVehicle)
@@ -7,3 +7,4 @@ admin.site.register(Make)
 admin.site.register(Model)
 admin.site.register(MyUser)
 admin.site.register(Log)
+admin.site.register(Message)
