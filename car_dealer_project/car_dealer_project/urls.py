29c8f3f74269e5061e596ae47525f43ec4efd7e9
boris tumbev <b.tumbev@code-nest.com>
2018-08-13 17:49:52 +0300

    added url pattern for static files

--- car_dealer_project/car_dealer_project/urls.py
+++ car_dealer_project/car_dealer_project/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
